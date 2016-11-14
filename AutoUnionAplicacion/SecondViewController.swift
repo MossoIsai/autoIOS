a850b7b64188338e17d8cb7a63dbb4336158420e
isaigarciamoso <ing.isaimosso@gmail.com>
2016-11-14 10:18:10 -0600

    Initial Commit

---
+++ AutoUnionAplicacion/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AutoUnionAplicacion
+//
+//  Created by Isai Garcia Moso on 14/11/16.
+//  Copyright © 2016 developerMosso. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
